ec72d95044e6139efcf4b3963ef24936a4259bf4
T.P.V.Krishna Teja <45790208+Teja-09@users.noreply.github.com>
2019-07-27 20:52:04 +0530

    designed front-end

--- src/eval.js
+++ src/eval.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./mycss.css";
+import "./mycss.css";
 
 class Eval extends React.Component
 {
@@ -10,12 +10,11 @@ class Eval extends React.Component
 
         this.state = {
             Name: "",
-            Email: "",
             Roll: "",
-            Email: "",
-            clg:"",
-            gpa: "",
+            ph: "",
             dob: "",
+            Email:"",
+            role: "",
         }
         this.sub = this.sub.bind(this);
         this.handle = this.handle.bind(this);
@@ -23,12 +22,9 @@ class Eval extends React.Component
 
     sub(event)
     {
-        let valid = true;
-
         if(!(/^[a-z\s]+$/i.test(this.state.Name)))
         {
             alert("Name should not contain numbers or special characters");
-            valid = false;
             event.preventDefault();
         }
 
@@ -42,7 +38,6 @@ class Eval extends React.Component
         {
             alert("College name should not contain numbers or special characters");
             event.preventDefault();
-            valid = false;
         }
 
         if(this.state.dob === "")
@@ -95,34 +90,23 @@ class Eval extends React.Component
     render(){
         return(
             <div className="whole">
-                <div className="containers"> 
+                <div className="container"> 
+                <div className="resume_h1"><h3><i>Resume</i></h3></div>
                 <form  action="http://localhost:8000/" className = "Form" onSubmit = {this.sub}  className="wholeform" method="POST" >
-                <div className="propose"><h2><i>Resume</i></h2></div>
-                <div>
-                </div>
                     <div className="formdiv">
-                    <label>1.Name:</label><br></br>
-                    <input type="text" placeholder="Enter your answer" name = "Name" value = {this.state.Name} onChange={(event) => this.handle(event)}/><br></br>
+                        <input type="text" placeholder="Name" name = "Name" value = {this.state.Name} onChange={(event) => this.handle(event)}/><br></br>
 
+                        <input type="text" placeholder="Address" name = "Roll" value = {this.state.Roll} onChange={(event) => this.handle(event)}/><br></br>
 
-                    <label>2.Roll number</label><br></br>
-                    <input type="text" placeholder="Enter your answer" name = "Roll" value = {this.state.Roll} onChange={(event) => this.handle(event)}/><br></br>
+                        <input type="text"  placeholder="Phone Number" name = "ph" value = {this.state.ph} onChange={(event) => this.handle(event)}/><br></br>
 
-                    <label>3.Email Address of PI:</label><br></br>
-                    <input type="text"  placeholder="Enter your answer" name = "Email" value = {this.state.Email} onChange={(event) => this.handle(event)}/><br></br>
+                        <input type="date"  placeholder="DOB " name = "dob" value = {this.state.dob} onChange={(event) => this.handle(event)}/><br></br>
 
-                    <label>4.CGPA</label><br></br>
-                    <input type="text"  placeholder="Enter your answer" name = "gpa" value = {this.state.gpa} onChange={(event) => this.handle(event)}/><br></br>
+                        <input type="text"  placeholder="Email" name = "Email" value = {this.state.Email} onChange={(event) => this.handle(event)}/><br></br>
 
-                    <label>5.College name</label><br></br>
-                    <input type="text"  placeholder="Enter your answer" name = "clg" value = {this.state.clg} onChange={(event) => this.handle(event)}/><br></br>
-
-                    <label>6.Date of Birth:</label><br></br>
-                    
-                    <input type="date"  placeholder="Please input date in format " name = "dob" value = {this.state.dob} onChange={(event) => this.handle(event)}/><br></br>
+                        <input type="text"  placeholder="Role" name = "role" value = {this.state.role} onChange={(event) => this.handle(event)}/><br></br>
                     </div>
-                <br></br>
-                <input className="subbutton" type = "submit" value="AddRecord" onClick= {this.sub}/>
+                    <input className="subbutton" type = "submit"  value="Submit" onClick= {this.sub}/>
                 </form>
             </div>
         </div>
--- src/post.js
+++ src/post.js
@@ -1,55 +1,27 @@
-// var express = require('express');
-// var app = express();
 
-// app.use(express.urlencoded());
-
-// app.get('/',function(req,res){
-//     res.send('hello world from GET');
-// })
-
-// app.post('/',function(req,res){
-    
-//     res.send(<h1>"username : " + req.param('Name',null)</h1>)
-//     // res.send("Roll : " + req.param('Roll',null))
-//     // res.send("Email : " + req.param('Email',null))
-//     // res.send("gpa : " + req.param('gpa',null))
-//     // res.send("clg : " + req.param('clg',null))
-//     // res.send("dob : " + req.param('dob',null))
-// })
-// app.listen(8000);
-
-
-
-var fs = require('fs');
 var express = require('express');
-// var formidable = require('formidable');
-var url=require('url');
-var bodyParser = require('body-parser');
-
-
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.urlencoded());
 
-app.get('/', function (req, res){
-
-  // res.sendFile(__dirname + '/Ptry.js');
-//   var q=url.parse(req.url,true);
-//     var name1=q.query;
-//     var y=name1.school;
-//     res.write(y);.
+app.get('/',function(req,res){
     res.send('hello world from GET');
-});
-app.post('/',function(req,res)
-{
-    var name=req.body.Name;
-    var roll=req.body.Roll;
-    var email=req.body.Email;
-    res.send("<h1>name</h1>"+name+"<h1>gender</h1>"+roll);
-    
-    
-});
+})
+
+app.post('/',function(req,res){
+    var name = req.body.Name;
+    var Roll = req.body.Roll;
+    var ph = req.body.ph;
+    var dob = req.body.dob;
+    var Email = req.body.Email;
+    var role = req.body.role;
+res.send(
+    "<div style=margin-left:600px> <h3 style=color:red><i style=float:left>Name : </i></h>" +name+"<h3 style=color:red><i style=float:left>Roll : </i></h>"+Roll+"<h3 style=color:red><i style=float:left>Email : </i></h>"+Email+
+"<h3 style=color:red><i style=float:left>GPA : </i></h>"+ph+"<h3 style=color:red><i style=float:left>College : </i></h>"+role+"<h1>Good,Your have a great resume</h1>"
+  )}  
+);
 
 
 app.listen(8000);
+
+ 
